221c1059cdfeff15f46a30310b5e7d7a093242b9
AzerothA <telifesite@gmail.com>
2023-01-15 23:22:06 +0330

    loading added

--- app/(components)/button.tsx
+++ app/(components)/button.tsx
@@ -2,6 +2,17 @@
 
 import styles from "./button.module.css";
 
-export default function Button({ children }: { children: JSX.Element }) {
-	return <button className={styles.button}>{children}</button>;
+export default function Button({
+	children,
+	disabled,
+}: { children: JSX.Element; disabled: boolean }) {
+	return (
+		<button
+			style={disabled ? { background: "rgb(52 52 52 / 32%)" } : {}}
+			disabled={disabled}
+			className={styles.button}
+		>
+			{children}
+		</button>
+	);
 }
--- app/form.tsx
+++ app/form.tsx
@@ -5,12 +5,14 @@ import { useRouter } from "next/navigation";
 import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function Form() {
+	const [loading, setLoading] = useState<boolean>(false);
 	const [path, setPath] = useState("");
 	const [month, setMonth] = useState("");
 	const router = useRouter();
 
 	const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
+		setLoading(true);
 
 		if (path !== "" && month !== "") {
 			router.push(`/repo/${path}/${month}`);
@@ -31,8 +33,9 @@ export default function Form() {
 				onChange={handleTextInputChange}
 			/>
 			<input type="month" name="x" id="" onChange={handleMonthInputChange} />
-			<Button>
-				<p>Go</p>
+
+			<Button disabled={loading}>
+				{loading ? <p>Calculating...</p> : <p>Calculate</p>}
 			</Button>
 		</form>
 	);
