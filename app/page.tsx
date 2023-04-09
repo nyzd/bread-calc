a9fce73f0abf8f90b3a531beeaabd4b05bcb43fd
AzerothA <telifesite@gmail.com>
2023-04-09 17:20:52 +0330

    quotes removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import Form from "./form";
 import styles from "./page.module.css";
-import RandomQuote from "./quotes";
 
 export default async function Page() {
 	return (
 		<div className={styles.page}>
 			<Form />
-			<div style={{ marginTop: "200px" }}>
-				<RandomQuote />
-			</div>
 		</div>
 	);
 }
