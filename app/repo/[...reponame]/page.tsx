c1a874b243c8bdd0850e6d4f3d201325d8f14e78
AzerothA <telifesite@gmail.com>
2023-07-27 21:41:32 +0330

    yet another update

--- app/repo/[...reponame]/page.tsx
+++ app/repo/[...reponame]/page.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
 import styles from "./styles.module.css";
 import { get_org, } from "./api_functions";
 import Scores, { ScoresFallback } from "./scores";
 import { Suspense } from "react";
+import git from "../../assets/g.svg";
+import followers from "../../assets/followers.svg";
 
 interface PageProps {
     params: {
@@ -9,11 +12,40 @@ interface PageProps {
     };
 }
 
+function OrgDetail({ org }: { org: any }) {
+    return (
+        <div className={styles.orgDetail}>
+            <Image
+                alt="Avatar"
+                src={org.avatar_url}
+                width={40}
+                height={40}
+            />
+            <h2>{org.login}</h2>
+
+            <div className={styles.orgInfo}>
+                <div>
+                    <Image width={20}
+                        height={20} alt="repos" src={git} />
+                    <p title="Repos">{org.public_repos}</p>
+                </div>
+
+                <div>
+                    <Image width={20}
+                        height={20} alt="followers" src={followers} />
+                    <p title="followers">{org.followers}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export default async function Reponame({ params: { reponame } }: PageProps) {
-    const org: any = await get_org(reponame[0]);
+    const org: any = reponame.length == 2 ? await get_org(reponame[0]) : null;
+
     return (
         <div>
-            <div></div>
+            {org ? <OrgDetail org={org} /> : ""}
             <div className={styles.scoreboard}>
                 <Suspense fallback={<ScoresFallback />}>
                     <Scores reponame={reponame} />
