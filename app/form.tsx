2a4bf5b78591e431bff205d51cc6587423194a00
AzerothA <telifesite@gmail.com>
2023-07-27 18:06:09 +0330

    Some thing changed :)

--- app/form.tsx
+++ app/form.tsx
@@ -5,38 +5,40 @@ import { useRouter } from "next/navigation";
 import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function Form() {
-	const [loading, setLoading] = useState<boolean>(false);
-	const [path, setPath] = useState("");
-	const [month, setMonth] = useState("");
-	const router = useRouter();
+    const [disabled, setDisabled] = useState<boolean>(true);
+    const [path, setPath] = useState("");
+    const [month, setMonth] = useState("");
+    const router = useRouter();
 
-	const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		setLoading(true);
+    const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-		if (path !== "" && month !== "") {
-			router.push(`/repo/${path}/${month}`);
-		}
-	};
+        if (path !== "" && month !== "") {
+            setDisabled(true);
+            router.push(`/repo/${path}/${month}`);
+        }
+    };
 
-	const handleTextInputChange = (e: ChangeEvent<HTMLInputElement>) =>
-		setPath(e.target.value);
+    const handleTextInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (disabled) setDisabled(false);
+        setPath(e.target.value);
+    }
 
-	const handleMonthInputChange = (e: ChangeEvent<HTMLInputElement>) =>
-		setMonth(e.target.value);
+    const handleMonthInputChange = (e: ChangeEvent<HTMLInputElement>) =>
+        setMonth(e.target.value);
 
-	return (
-		<form onSubmit={handleFormSubmit}>
-			<input
-				type="text"
-				placeholder="Repository URL"
-				onChange={handleTextInputChange}
-			/>
-			<input type="month" name="x" id="" onChange={handleMonthInputChange} />
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <input
+                type="text"
+                placeholder="Repository URL"
+                onChange={handleTextInputChange}
+            />
+            <input type="month" name="x" id="" onChange={handleMonthInputChange} />
 
-			<Button disabled={loading}>
-				{loading ? <p>Calculating...</p> : <p>Calculate</p>}
-			</Button>
-		</form>
-	);
+            <Button disabled={disabled}>
+                <h3>Calculate</h3>
+            </Button>
+        </form>
+    );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,33 +5,36 @@ import Header from "./(components)/header";
 import "./global.css";
 
 async function github_api_status() {
-	const begin = Date.now();
-	const response = await call("/octocat");
-	const delay = Date.now() - begin;
+    const begin = Date.now();
+    const response = await call("/octocat");
+    const delay = Date.now() - begin;
 
-	return [response.status, delay];
+    return [response.status, delay];
 }
 
 export default async function Layout({ children }: { children: JSX.Element }) {
-	const [github_status, delay] = await github_api_status();
-	return (
-		<>
-			<html lang="en">
-				<body>
-					<Header>
-						<h3>Bread Calculator</h3>
+    const [github_status, delay] = await github_api_status();
+    return (
+        <>
+            <html lang="en">
+                <head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                </head>
+                <body>
+                    <Header>
+                        <h3>Bread Calculator</h3>
 
-						{github_status === 200 ? (
-							<Status>
-								<h3 style={{ color: "#72f372" }}>{delay}ms</h3>
-							</Status>
-						) : (
-							<h3 style={{ color: "red" }}>Github api is Down</h3>
-						)}
-					</Header>
-					{children}
-				</body>
-			</html>
-		</>
-	);
+                        {github_status === 200 ? (
+                            <Status>
+                                <h3 style={{ color: "#72f372" }}>{delay}ms</h3>
+                            </Status>
+                        ) : (
+                            <h3 style={{ color: "red" }}>Github api is Down</h3>
+                        )}
+                    </Header>
+                    {children}
+                </body>
+            </html>
+        </>
+    );
 }
