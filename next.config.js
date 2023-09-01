221b68303f412884a4636d3e022ac2fec0a0dc29
AzerothA <telifesite@gmail.com>
2023-09-01 23:31:47 +0330

    Chache Disabled

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverActions: true ,
-  },
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
+    experimental: {
+        appDir: true,
+        serverActions: true,
+    },
+    images: {
+        domains: ['avatars.githubusercontent.com'],
+    },
+    headers: () => [
+        {
+            source: '/repo/:path*',
+            headers: [
+                {
+                    key: 'Cache-Control',
+                    value: 'no-store',
+                },
+            ],
+        },
+    ],
 }
 
 module.exports = nextConfig
